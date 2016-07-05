541745adf1863fd927031b52cd1f3193d0a805fe
ekolpakov <ekolpakov@greet-go.com>
2016-07-05 08:48:48 +0600

    varsion = 0.0.19 - Added ConsumerEventRegister.toString()

--- greetgo.kafka.parent/greetgo.kafka/src/kz/greetgo/kafka/events/e/ConsumerEventRegister.java
+++ greetgo.kafka.parent/greetgo.kafka/src/kz/greetgo/kafka/events/e/ConsumerEventRegister.java
@@ -3,10 +3,18 @@ package kz.greetgo.kafka.events.e;
 import kz.greetgo.kafka.consumer.ConsumerDefinition;
 import kz.greetgo.kafka.events.KafkaEvent;
 
+import java.text.SimpleDateFormat;
+
 public class ConsumerEventRegister extends KafkaEvent {
   public final ConsumerDefinition consumerDefinition;
 
   public ConsumerEventRegister(ConsumerDefinition consumerDefinition) {
     this.consumerDefinition = consumerDefinition;
+  }
+
+  @Override
+  public String toString() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+    return sdf.format(at) + ' ' + name() + ' ' + consumerDefinition;
   }
 }
