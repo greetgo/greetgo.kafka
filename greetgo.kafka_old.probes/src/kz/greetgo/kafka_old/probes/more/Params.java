b14aaf9627727ae995b910a0f92da458759196e4
ekolpakov <ekolpakov@greet-go.com>
2019-11-06 10:57:52 +0600

    Удалён старый модуль

--- greetgo.kafka_old.probes/src/kz/greetgo/kafka_old/probes/more/Params.java
+++
@@ -1,7 +0,0 @@
-package kz.greetgo.kafka_old.probes.more;
-
-public class Params {
-  public static final String TOPIC_NAME = "pomidor";
-  public static final int TOPIC_PARTITIONS = 10;
-  public static final int TOPIC_REPLICATION_FACTOR = 1;
-}
